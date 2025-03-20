4934403cfa1321328abc81a5cdc4934de5504ed4
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-03-20 20:23:56 +0100

    feat(leader): validate worker certificate with a request

--- leader/src/validate.rs
+++ leader/src/validate.rs
@@ -1,14 +1,48 @@
+use std::{fs, path::PathBuf};
+
 use async_trait::async_trait;
 use clap::Parser;
+use reqwest::{Certificate, Url};
 use shared::Runnable;
 
 #[derive(Parser)]
 #[command(about = "Starts the leader")]
-pub struct ValidateArgs {}
+pub struct ValidateArgs {
+    /// Worker server address.
+    #[arg(short, long)]
+    pub address: Url,
+
+    /// Path to the Root CA certificate file.
+    #[arg(long)]
+    pub ca_cert: PathBuf,
+}
 
 #[async_trait]
 impl Runnable for ValidateArgs {
     async fn run(&self) -> Result<(), anyhow::Error> {
-        todo!()
+        tracing::info!(address = %self.address, "Validating the worker TLS...");
+
+        let client = build_client(&self.ca_cert)?;
+        let response = client
+            .get(self.address.clone())
+            .send()
+            .await?
+            .error_for_status()?;
+
+        let text = response.text().await?;
+        tracing::info!(%text, "Connection successfull!");
+
+        Ok(())
     }
+}
+
+fn build_client(ca_cert: &PathBuf) -> anyhow::Result<reqwest::Client> {
+    let ca_cert_pem = fs::read(ca_cert)?;
+    let ca_cert = Certificate::from_pem(&ca_cert_pem)?;
+
+    let client = reqwest::Client::builder()
+        .add_root_certificate(ca_cert)
+        .build()?;
+
+    Ok(client)
 }
