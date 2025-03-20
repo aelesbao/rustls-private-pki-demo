2f0f2ad3b3b9190fa1983bdb8b38c51fe1880628
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-03-20 14:06:21 +0100

    feat(worker): start tls server with signed certs

--- worker/src/server.rs
+++ worker/src/server.rs
@@ -1,13 +1,37 @@
+use std::path::PathBuf;
+
+use async_trait::async_trait;
 use clap::Parser;
 use shared::Runnable;
 
 #[derive(Parser)]
 #[command(about = "Starts the worker")]
-pub struct ServerArgs {}
+pub struct ServerArgs {
+    /// Path to the signed server certificate file.
+    #[arg(long)]
+    pub cert: PathBuf,
+
+    /// Path to the server private key file.
+    #[arg(long)]
+    pub key: PathBuf,
+}
 
 #[async_trait]
 impl Runnable for ServerArgs {
     async fn run(&self) -> anyhow::Result<()> {
+        tracing::info!("Starting the worker server...");
+
+        use warp::Filter;
+
+        let routes = warp::any().map(|| "OK");
+
+        warp::serve(routes)
+            .tls()
+            .cert_path(&self.cert)
+            .key_path(&self.key)
+            .run(([127, 0, 0, 1], 3030))
+            .await;
+
         Ok(())
     }
 }
